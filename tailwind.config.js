dbb5d1f2ff4559e21ffbcdacb2b209335abfee58
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-10-19 21:22:16 -0500

    ultimate

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,10 @@ module.exports = {
         'do-blue-light': 'rgb(0, 105, 255)',
         'do-blue-medium':'rgb(0, 86,255)',
         'do-blue-mediumdark':'#1d274c'
+      },
+      boxShadow:{
+        'input':'0 5px 1px 0 rgb(0,0,0, 0.1)',
+        'input-focus':'0 2px 1px 0 rgb(0,0,0, 0.1)',
       }
     },
   },
